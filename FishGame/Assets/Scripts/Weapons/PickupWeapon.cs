233c6300f0eb2b913f0374d7d7be08f7fbc009b5
simodewit <s.f.buikema@gmail.com>
2024-02-28 14:42:01 +0100

    throwing of objects
    i have improved the throwing. there are some things still to be tweaked but you can now throw without issues

--- FishGame/Assets/Scripts/Weapons/PickupWeapon.cs
+++ FishGame/Assets/Scripts/Weapons/PickupWeapon.cs
@@ -8,26 +8,25 @@ public class PickupWeapon : MonoBehaviour
     [Tooltip("The object it has to instantiate")]
     public GameObject objectToInst;
 
-    //private GameObject colObject;
+    private GameObject colObject;
 
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    XRGrabInteractable grab = other.transform.GetComponent<XRGrabInteractable>();
+    public void OnTriggerEnter(Collider other)
+    {
+        XRGrabInteractable grab = other.transform.GetComponent<XRGrabInteractable>();
 
-    //    if (grab != null)
-    //    {
-    //        colObject = other.gameObject;
-    //    }
-    //}
+        if (grab != null)
+        {
+            colObject = other.gameObject;
+        }
+    }
 
-    //public void OnTriggerExit(Collider other)
-    //{
-    //    colObject = null;
-    //}
+    public void OnTriggerExit(Collider other)
+    {
+        colObject = null;
+    }
 
     public void InstObject(ActivateEventArgs args)
     {
-        Vector3 a = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z);
-        GameObject obj = Instantiate(objectToInst, a, Quaternion.identity);
+
     }
 }
--- FishGame/Assets/Scripts/Weapons/Throwable.cs
+++ FishGame/Assets/Scripts/Weapons/Throwable.cs
@@ -1,24 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Throwable : MonoBehaviour
 {
+    #region variables
+
     [Tooltip("The damage that the throwable does to the enemy")][Range(0, 100)]
     public int damage = 20;
-    [Tooltip("The speed at wich the throwable is traveling")][Range(0, 100)]
-    public int speed;
     [Tooltip("The collider of this object")]
     public Collider col;
+    [Tooltip("The rigidbody of this object")]
+    public Rigidbody rb;
+    [Tooltip("The time that the collider of this throwable is dissabled after letting go of the object. you want this to be as low as possible without colliding with the player")][Range(0,3)]
+    public float time = 1;
+    [Tooltip("If this throwable has one side that should be in front this box should be checked")]
+    public bool hasFront;
+    [Tooltip("The speed thats needed to turn the object if hasFront is enabled")][Range(0, 20)]
+    public float turnSpeed = 5;
 
-    public void DisableCol()
+    private bool activateTimer;
+    private bool isPickedUp;
+    private float timer;
+
+    #endregion
+
+    #region start and update
+
+    public void Start()
     {
-        col.enabled = false;
+        timer = time;
     }
 
-    public void EnableCol()
+    public void Update()
     {
-        col.enabled = true;
+        Timer();
+        FaceDirection();
+    }
+
+    #endregion
+
+    #region timer
+
+    public void Timer()
+    {
+        if (activateTimer)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                activateTimer = false;
+                col.enabled = true;
+                timer = time;
+            }
+        }
+    }
+
+    #endregion
+
+    #region direction code
+
+    public void FaceDirection()
+    {
+        if (hasFront)
+        {
+            if (isPickedUp)
+            {
+                transform.localRotation = new Quaternion(90, 0, 0, 0);
+            }
+            else
+            {
+                if (rb.velocity.magnitude >= turnSpeed)
+                {
+                    transform.LookAt(rb.velocity);
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region extra code
+
+    public void PickupThrowable()
+    {
+        col.enabled = false;
+        isPickedUp = true;
+    }
+
+    public void DropThrowable()
+    {
+        activateTimer = true;
+        isPickedUp = false;
     }
 
     public void OnCollisionEnter(Collision collision)
@@ -31,4 +106,6 @@ public class Throwable : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    #endregion
 }
